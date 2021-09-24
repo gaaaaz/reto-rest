04e7332e5da94fa5b70e4d39e49694f970efb0c2
Christian <arturogarcia_96@hotmail.com>
2021-09-24 13:57:31 -0500

    commit inicial

---
+++ src/main/java/com/entelgy/retorest/RetoRestApplication.java
@@ -0,0 +1,13 @@
+package com.entelgy.retorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entelgy/retorest/RetoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entelgy.retorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
