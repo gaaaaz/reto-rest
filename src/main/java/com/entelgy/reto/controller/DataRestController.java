b461a659022b2df43ba6d64d717524a26d90ddf7
Christian <arturogarcia_96@hotmail.com>
2021-09-24 15:17:34 -0500

    unit test

--- src/main/java/com/entelgy/reto/controller/DataRestController.java
+++ src/main/java/com/entelgy/reto/controller/DataRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -30,7 +31,7 @@ public class DataRestController {
 	@Value("${com.entelgy.url}")
 	private String url;
 	
-	@GetMapping("/all")
+	@PostMapping("/all")
 	public ResponseEntity<String> getDataStructure(){
 		
 		RestTemplate restTemplate = new RestTemplate();
